1e2021a5ba7182b5c940b1a9319281547bc7b7fe
yetto-tools <ergonzalez209@gmail.com>
2022-10-27 22:14:20 -0600

    Add Message to cosi

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -11,9 +11,7 @@ export const Home = () => {
             <div className="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
               <div className="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
                 <div className="home__welcome markdown">
-                  <h1>
-                    Yo, I’m Bluise - A Nuxt.js &amp; Netlify CMS boilerplate.
-                  </h1>
+                  <h1>Mesaje de Pruebas para la cosi</h1>
                   <p>
                     Authentic cred hoodie hashtag selfies. Raclette banjo
                     cardigan cred. Kitsch XOXO lomo polaroid. Knausgaard put a
