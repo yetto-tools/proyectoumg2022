5a6621cc8392e30f69cb8a312988399335e9678a
yettol-tools <egonzalez209@gmail.com>
2022-10-29 10:57:23 -0600

    add animation navbar and color backgroun link

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -7,7 +7,7 @@ export const NavBar = () => {
     <header className='flex w-full items-center sm:my-10 md:my-12'>
       <nav className='nav fixed ml-auto w-full bg-gray-400/30 pb-2 shadow-md shadow-black/30  backdrop-blur-sm max-sm:fixed max-sm:bottom-0 sm:top-0'>
         <ul className='flex flex-row max-sm:justify-evenly sm:mt-2 sm:space-x-8 sm:px-4 sm:pt-2 md:mt-0 md:mr-4 md:pt-2 lg:mr-8 lg:pt-1'>
-          <li className='block px-1 py-1 font-medium md:p-2 lg:px-4'>
+          <li className='block px-1 py-1 font-medium hover:scale-110 hover:cursor-pointer hover:bg-white/80 hover:text-black/70 md:p-2 lg:px-4'>
             <Link to='/'>
               <span className='mt-2 inline-flex text-center lg:mx-0 lg:px-8'>
                 <span className='max-sm:space-x-12'>
@@ -17,7 +17,7 @@ export const NavBar = () => {
               </span>
             </Link>
           </li>
-          <li className='block px-1 py-1 font-medium md:p-2 lg:px-4'>
+          <li className='block px-1 py-1 font-medium hover:scale-110 hover:cursor-pointer hover:bg-white/80 hover:text-black/70 md:p-2 lg:px-4'>
             <Link to='/contact'>
               <span className='mt-2 inline-flex text-center lg:mx-0 lg:px-8'>
                 <span className='max-sm:space-x-12'>
@@ -27,7 +27,7 @@ export const NavBar = () => {
               </span>
             </Link>
           </li>
-          <li className='block px-1 py-1 font-medium md:p-2 lg:px-4'>
+          <li className='block px-1 py-1 font-medium hover:scale-110 hover:cursor-pointer hover:bg-white/80 hover:text-black/70 md:p-2 lg:px-4'>
             <Link to='/about'>
               <span className='mt-2 inline-flex text-center lg:mx-0 lg:px-8'>
                 <span className='max-sm:space-x-12'>
